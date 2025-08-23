471f2ca1cbe3aff9d8114cef3a74397dddc432d7
Daniel <danieldcc.colares@gmail.com>
2025-08-23 14:44:45 -0300

    feat: adiciona tabela de matriculas

---
+++ drizzle/0004_adorable_beast.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "enrollments" DROP CONSTRAINT "enrollments_userID_users_id_fk";
+--> statement-breakpoint
+ALTER TABLE "enrollments" ADD COLUMN "userId" uuid NOT NULL;--> statement-breakpoint
+ALTER TABLE "enrollments" ADD CONSTRAINT "enrollments_userId_users_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "enrollments" DROP COLUMN "userID";
